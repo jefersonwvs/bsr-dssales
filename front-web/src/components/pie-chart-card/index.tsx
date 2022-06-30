7f9335ed6ab154ed64587a791feb8940e0c9b41b
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2022-06-29 21:45:21 -0400

    PieChartCard component

---
+++ front-web/src/components/pie-chart-card/index.tsx
@@ -0,0 +1,28 @@
+import './styles.css';
+
+import ReactApexChart from 'react-apexcharts';
+import { buildPieChartConfig } from './helpers';
+
+type Props = {
+  labels: string[];
+  name: string;
+  series: number[];
+};
+
+function PieChartCard(props: Props) {
+  const { labels, name, series } = props;
+
+  return (
+    <div className="pie-chart-card base-card">
+      <ReactApexChart
+        options={buildPieChartConfig(labels, name)}
+        type="donut"
+        width="400"
+        height="400"
+        series={series}
+      />
+    </div>
+  );
+}
+
+export default PieChartCard;
